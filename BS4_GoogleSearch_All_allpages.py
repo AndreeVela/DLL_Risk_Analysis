8df4e6a9a58bacd97528090009035d53660cb970
Navoneel Chakrabarty <navods000@gmail.com>
2022-11-08 11:33:32 +0100

    merged from navo_roya
    3 files:
    
    1. BS4_GoogleSearch_All.py: search results from specified number of pages
    
    2. BS4_GoogleSearch_All_allpages: search results from all pages
    
    3. library: folder containing supporting files with modules

---
+++ BS4_GoogleSearch_All_allpages.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# importing necessary libraries
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+from googletrans import Translator, constants
+import numpy as np
+
+# importing user-defined libraries
+from library.link_preprocessor import *
+
+
+# In[2]:
+
+
+# inputs
+# entity = "Strukton"
+#n_pages = 4
+
+
+# In[3]:
+
+
+# building the search query
+query = '(“Strukton”) AND scandal OR fraud OR investigation OR investigate OR litigation OR crime OR arrest OR allege OR guilty OR illegal OR indict OR terrorism OR terrorist OR smuggle OR smuggling OR corruption OR testify OR racketeer OR incriminate OR mafia OR convicted OR conviction OR accused OR defraud OR controversy OR controversial OR jail'
+
+
+# In[4]:
+
+
+# dummying the header to provide the functionality of a bot to the web-crawler
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
+
+r = requests.get("https://www.google.com/search?q=" + query, headers = headers)
+
+
+# In[5]:
+
+
+# getting the text response and parsing HTML
+c = r.text
+soup = BeautifulSoup(c, "html.parser")
+
+
+# In[6]:
+
+
+# getting all the google searches and related data
+search_data = soup.find_all("div", {"class" : "g"})
+
+
+# In[7]:
+
+
+# scraping all the search links appearing in the number of pages requested in the input
+
+all_search_dict = {'URL': [], 'Date' : []}
+page_no = 1
+while(1):
+    n_searches = len(search_data)
+    for search in range(n_searches):
+        search_result = search_data[search]
+        ## getting the search URL
+        url = search_result.find("a").get("href")
+        all_search_dict['URL'].append(url)
+
+        try:
+            ## trying to get the search date
+            date_section = search_result.find('span', {"class":"MUxGbd wuQ4Ob WZ8Tjf"}).find_all('span')
+            for element in date_section:
+                text_element = element.text
+                if date_checker(text_element):
+                    date = text_element
+                    break
+        except:
+            ## if no search date can be found...
+            date = ""
+            
+        all_search_dict['Date'].append(date)
+    
+    ## navigating to the next page and scraping the subsequent google search URLs
+    try:
+        search_page = "Page " + str(page_no + 1)
+        search_url = soup.find("a", {"aria-label" : search_page}).get("href")
+        r = requests.get("https://www.google.com" + search_url, headers = headers)
+        c = r.text
+        soup = BeautifulSoup(c, "html.parser")
+        search_data = soup.find_all("div", {"class" : "g"})
+    except:
+        # no more pages
+        break
+
+
+# In[8]:
+
+
+# converting the dictionary containing URL and Timestamp for each link into a DataFrame
+search_df = pd.DataFrame(all_search_dict)
+
+
+# In[9]:
+
+
+# getting the Source and Headline from the URL
+search_df['Source'] = search_df['URL'].apply(extract_sourcename)
+search_df['Heading'] = search_df['URL'].apply(extract_headline)
+
+
+# In[10]:
+
+
+# compiling the search dataframe
+#search_df.head()
+
+
+# In[11]:
+
+
+# reading the date and applying date processing
+search_df['Date'] = search_df['Date'].apply(date_processor)
+#search_df.head()
+
+
+# In[12]:
+
+
+search_df.to_excel('Google_Search_All.xlsx', index = False)
+
---
+++ library/link_preprocessor.py
@@ -0,0 +1,113 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+from googletrans import Translator, constants
+import numpy as np
+
+
+
+# extracting the source of each of the URLs
+def extract_sourcename(url):
+    source_name = url.split('//')[1].split('/')[0]
+    return source_name
+    ## iff the source is a part of the entity's home (company) website, the URL is NOT CONSIDERED FOR AMS
+    #if 'strukton' in source_name:
+    #    return False
+    #else:
+    #    return source_name
+    
+    
+# extracting the headline of each of the URLs
+def extract_headline(url):
+    i = 1
+    while(1):
+        if url.split('/')[-i] != '':
+            headline = url.split('/')[-i]
+            break
+        else:
+            i += 1
+            
+    headline = ' '.join(headline.split('-'))
+    headline = headline[0].upper() + headline[1:]
+    headline = re.sub('[^A-Za-z ]+', '', headline)
+    
+    # translating the headlines to English
+    translator = Translator()
+    headline_english = translator.translate(headline, dest='en').text
+    return headline_english
+
+
+def date_checker(text):
+    # checking whether a 4-digit year
+    if text.split()[-1].isnumeric() and int(text.split()[-1]) >= 1000:
+        return True
+    elif 'ago' in text:
+        return True
+    else:
+        return False
+
+def date_processor(date):
+    month_to_num = {'jan.':'01',
+                    'feb.':'02',
+                    'mar.':'03',
+                    'mrt.':'03',
+                    'apr.':'04',
+                    'may.':'05',
+                    'mei' :'05',
+                    'jun.':'06',
+                    'juni':'06',
+                    'jul.':'07',
+                    'juli':'07',
+                    'aug.':'08',
+                    'sep.':'09',
+                    'oct.':'10',
+                    'okt.':'10',
+                    'nov.':'11',
+                    'dec.':'12'}
+    translator = Translator()
+    date_english = translator.translate(date, dest='en').text
+    flag = 0
+    if 'ago' in date_english:
+        # if they are 'some hours/days/minutes/seconds ago'
+        flag = 1
+        try:
+            time_day = date_english.split()[1]
+            delta_time = date_english.split()[0]
+            if 'hour' in time_day:
+                # subtract hours from real-time and get the date
+                actual_date = (pd.Timestamp.now() - pd.Timedelta(hours=delta_time)).date().strftime("%d/%m/%Y")
+            elif 'minute' in time_day:
+                # subtract minutes from real-time and get the date
+                actual_date = (pd.Timestamp.now() - pd.Timedelta(minutes=delta_time)).date().strftime("%d/%m/%Y")
+            elif 'second' in time_day:
+                # subtract seconds from real-time and get the date
+                actual_date = (pd.Timestamp.now() - pd.Timedelta(seconds=delta_time)).date().strftime("%d/%m/%Y")
+            elif 'day' in time_day:
+                # subtract days from real-time and get the date
+                actual_date = (pd.Timestamp.now() - pd.Timedelta(days=delta_time)).date().strftime("%d/%m/%Y")
+        except:
+            flag = 0
+    
+    if len(date_english.split()) == 3:
+        # day, month and year all are present
+        flag = 1
+        actual_date = date_english.split()[0] + '/' + month_to_num[date_english.split()[1]] + '/' + date_english.split()[2]
+    
+    elif len(date_english.split()) == 2:
+        # only month and year are present
+        flag = 1
+        actual_date = month_to_num[date_english.split()[0]] + '/' + date_english.split()[1]
+        
+    elif len(date_english.split()) == 1:
+        # only year is present
+        flag = 1
+        actual_date = date_english.split()[0]
+        
+    else:
+        # unidentified date
+        actual_date = np.nan
+        flag = 0
+
+    # returns datetime format of yyyy-mm-dd
+    return pd.to_datetime(actual_date, dayfirst = True)
